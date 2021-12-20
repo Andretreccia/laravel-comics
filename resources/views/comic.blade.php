243a6cdc92623c307ba94b258a4e17256be2177f
Andretreccia <andreabtreccia@gmail.com>
2021-12-20 17:05:24 +0100

    add the details of the single comic dynamically

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -21,17 +21,49 @@
     <div class="container">
         <div class="col-6">
             <h6>Talent</h6>
-            <hr>
             <div>
                 <div>Art by:</div>
-                @foreach ($comic as $artists)
-                @foreach ($artists as $artist)
+                @foreach ($comic['artists'] as $artist)
                 <a href="">{{$artist}}</a>
                 @endforeach
+            </div>
+            <div>
+                <div>Written by:</div>
+                @foreach ($comic['writers'] as $writer)
+                <a href="">{{$writer}}</a>
                 @endforeach
             </div>
+
         </div>
-        <div class="col-6"></div>
+        <div class="col-6">
+            <div>
+                <div>
+                    Series:
+                </div>
+                <div>
+                    {{$comic['series']}}
+                </div>
+            </div>
+            <div>
+                <div>
+                    U.S. Price:
+                </div>
+                <div>
+                    {{$comic['price']}}
+                </div>
+            </div>
+            <div>
+                <div>
+                    On Sale Date:
+                </div>
+                <div>
+                    {{$comic['sale_date']}}
+                </div>
+            </div>
+
+
+
+        </div>
     </div>
 </div>
 
